adf00e1e47f8355feaee94f6accaead585c167ce
Teddysir <sdzx0719@naver.com>
2024-12-11 19:05:37 +0900

    feat: 올바르지 않은 보너스 번호 형식 입력시 예외 처리

--- src/main/java/lotto/constants/ErrorMessageType.java
+++ src/main/java/lotto/constants/ErrorMessageType.java
@@ -2,7 +2,8 @@ package lotto.constants;
 
 public enum ErrorMessageType {
 
-    ERROR_NULL("[ERROR] 빈값은 입력할 수 없습니다. 다시 입력 해주세요.");
+    ERROR_NULL("[ERROR] 빈값은 입력할 수 없습니다. 다시 입력 해주세요."),
+    ERROR_BONUS_NUMBER_FORMAT("[ERROR] 올바르지 않은 보너스번호 형식입니다. 다시 입력 해주세요.");
 
     private final String message;
 
--- src/main/java/lotto/utils/ServiceValidation.java
+++ src/main/java/lotto/utils/ServiceValidation.java
@@ -2,7 +2,11 @@ package lotto.utils;
 
 import lotto.constants.ErrorMessageType;
 
+import java.util.regex.Pattern;
+
 public class ServiceValidation {
+
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("-?\\d+");
 
     public static void validateNull(String input) {
         if (input.isBlank()) {
@@ -10,7 +14,11 @@ public class ServiceValidation {
         }
     }
 
-
+    private static void validateIsNumericBonusNumber(String input) {
+        if (!NUMBER_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException(ErrorMessageType.ERROR_BONUS_NUMBER_FORMAT.getMessage());
+        }
+    }
 
 
 }
