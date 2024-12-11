4ea1fb6d6fae457dea8660ea66908fbde0892407
Teddysir <sdzx0719@naver.com>
2024-12-11 19:13:33 +0900

    feat: 올바르지 않은 번호 형식일 경우 예외 처리

--- src/main/java/lotto/constants/ErrorMessageType.java
+++ src/main/java/lotto/constants/ErrorMessageType.java
@@ -3,7 +3,8 @@ package lotto.constants;
 public enum ErrorMessageType {
 
     ERROR_NULL("[ERROR] 빈값은 입력할 수 없습니다. 다시 입력 해주세요."),
-    ERROR_BONUS_NUMBER_FORMAT("[ERROR] 올바르지 않은 보너스번호 형식입니다. 다시 입력 해주세요.");
+    ERROR_BONUS_NUMBER_FORMAT("[ERROR] 올바르지 않은 보너스번호 형식입니다. 다시 입력 해주세요."),
+    ERROR_LOTTO_NUMBER_FORMAT("[ERROR] 올바르지 않은 로또번호 형식입니다. 다시 입력 해주세요.");
 
     private final String message;
 
@@ -14,4 +15,5 @@ public enum ErrorMessageType {
     public String getMessage() {
         return message;
     }
-    }
+
+}
--- src/main/java/lotto/utils/ServiceValidation.java
+++ src/main/java/lotto/utils/ServiceValidation.java
@@ -2,7 +2,10 @@ package lotto.utils;
 
 import lotto.constants.ErrorMessageType;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class ServiceValidation {
 
@@ -14,11 +17,25 @@ public class ServiceValidation {
         }
     }
 
-    private static void validateIsNumericBonusNumber(String input) {
+    public static void validateIsNumericBonusNumber(String input) {
         if (!NUMBER_PATTERN.matcher(input).matches()) {
             throw new IllegalArgumentException(ErrorMessageType.ERROR_BONUS_NUMBER_FORMAT.getMessage());
         }
     }
+
+    public static List<Integer> validateIsNumericLottoNumbers(String rawInput) {
+        try {
+            List<Integer> lottoNumbers = Arrays.stream(rawInput.split(","))
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .toList();
+
+            return lottoNumbers;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessageType.ERROR_LOTTO_NUMBER_FORMAT.getMessage());
+        }
+    }
+
 
 
 }
